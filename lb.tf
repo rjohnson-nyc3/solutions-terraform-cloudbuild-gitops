24c312499f65b2288f51675401cb1e30b0788830
Robert Johnson <rjohnson@cyber.nyc.gov>
2021-09-13 10:59:50 -0400

    Moved the static IP's to the network file

--- lb.tf
+++ lb.tf
@@ -1,28 +1,4 @@
-# Load Balancer3
-resource "google_compute_address" "nyc3_dev_avi_internal_static_ip_1" {
-  name         = "nyc3-dev-avi-internal-static-ip-1"
-  project      = var.network_project_id
-  subnetwork   = "projects/nyc3-dev-hostnetwork/regions/us-east1/subnetworks/net-10-10-100-0-24"
-  address_type = "INTERNAL"
-  address      = "10.10.100.11"
-  region       = var.primary_region
-}
-resource "google_compute_address" "nyc3_dev_avi_internal_static_ip_2" {
-  name         = "nyc3-dev-avi-internal-static-ip-2"
-  project      = var.network_project_id
-  subnetwork   = "projects/nyc3-dev-hostnetwork/regions/us-east1/subnetworks/net-10-10-100-0-24"
-  address_type = "INTERNAL"
-  address      = "10.10.100.12"
-  region       = var.primary_region
-}
-resource "google_compute_address" "nyc3_dev_avi_internal_static_ip_3" {
-  name         = "nyc3-dev-avi-internal-static-ip-3"
-  project      = var.network_project_id
-  subnetwork   = "projects/nyc3-dev-hostnetwork/regions/us-east1/subnetworks/net-10-10-100-0-24"
-  address_type = "INTERNAL"
-  address      = "10.10.100.13"
-  region       = var.primary_region
-}
+# Load Balancers
 
 resource "google_compute_instance" "nyc3_dev_avi_lb_controller_1" {
   
--- network.tf
+++ network.tf
@@ -1,4 +1,29 @@
 # Network Configurations
+resource "google_compute_address" "nyc3_dev_avi_internal_static_ip_1" {
+  name         = "nyc3-dev-avi-internal-static-ip-1"
+  project      = var.network_project_id
+  subnetwork   = "projects/nyc3-dev-hostnetwork/regions/us-east1/subnetworks/net-10-10-100-0-24"
+  address_type = "INTERNAL"
+  address      = "10.10.100.11"
+  region       = var.primary_region
+}
+resource "google_compute_address" "nyc3_dev_avi_internal_static_ip_2" {
+  name         = "nyc3-dev-avi-internal-static-ip-2"
+  project      = var.network_project_id
+  subnetwork   = "projects/nyc3-dev-hostnetwork/regions/us-east1/subnetworks/net-10-10-100-0-24"
+  address_type = "INTERNAL"
+  address      = "10.10.100.12"
+  region       = var.primary_region
+}
+resource "google_compute_address" "nyc3_dev_avi_internal_static_ip_3" {
+  name         = "nyc3-dev-avi-internal-static-ip-3"
+  project      = var.network_project_id
+  subnetwork   = "projects/nyc3-dev-hostnetwork/regions/us-east1/subnetworks/net-10-10-100-0-24"
+  address_type = "INTERNAL"
+  address      = "10.10.100.13"
+  region       = var.primary_region
+}
+
 
 # resource "google_compute_network" "nyc3_dev_sbx_vpc" {
 #   name                    = var.vpc_network
